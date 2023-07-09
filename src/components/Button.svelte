29707d210b7cf26d29fba097ae66a3d11add2d17
dek <polydek@googlemail.com>
2023-07-09 23:25:53 +0200

    scope + tailwind fixes

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -3,8 +3,15 @@
   export let col = 'sky'
   export let css = ''
   export let click = ()=>{}
+
+  const cols = {
+    sky   : 'bg-sky-500 hover:bg-sky-700',
+    indigo: 'bg-indigo-500 hover:bg-indigo-700',
+    slate : 'bg-slate-500 hover:bg-slate-700'
+  }[col]
+
 </script>
 
 <button
   on:click={click}
-  class="text-white font-semibold py-2 text-sm bg-{col}-500 hover:bg-{col}-700 w-full rounded-md {css}">{text}</button>
+  class="text-white font-semibold py-2 text-sm {cols} w-full rounded-md {css}">{text}</button>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 export default {
   content: [
     './index.html',
-    './src/**/*.{svelte,html,js,ts,jsx,tsx}'
+    './src/**/*.{svelte,js,ts,jsx,tsx}'
   ],
   theme: {
     extend: {}
